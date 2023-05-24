bbbf4a58887798d6dc9d18aad90f001bc8e6ad79
GianluGiarde <gianlugiarde@gmail.com>
2023-05-24 18:07:38 +0200

    import: store / add: template

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,24 @@
 <script>
-export default {};
+import store from "../store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <header>
+    <img src="" alt="" />
+    <input
+      type="text"
+      v-model="store.imputSearch"
+      @keyup.enter="$emit('searchMovies', store.imputSearch)"
+    />
+    <button @click="$emit('searchMovies', store.imputSearch)">Cerca</button>
+  </header>
+</template>
 
 <style lang="scss" scoped></style>
