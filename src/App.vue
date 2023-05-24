80264ebd8218531bb8fbc627adccd4011bd9f2c2
GianluGiarde <gianlugiarde@gmail.com>
2023-05-24 18:05:36 +0200

    add: getmovies method / fix: components

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,33 @@
 import store from "./store";
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
+import axios from "axios";
+
 export default {
   data() {
     return {
       store,
-      Header,
-      Main,
     };
+  },
+  components: {
+    Header,
+    Main,
+  },
+  methods: {
+    getMovies(inputSearch) {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/search/movie?api_key=4ffc9c2cabb7a0565658597d4cddb688&query=" +
+            inputSearch
+        )
+        .then((response) => (this.store.movies = response.data.results));
+    },
   },
 };
 </script>
 
 <template>
-  <Header />
+  <Header @searchMovies="getMovies" />
   <Main />
 </template>
 
