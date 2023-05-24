ffee07441199899150f83889848f2a64c43673da
GianluGiarde <gianlugiarde@gmail.com>
2023-05-24 18:09:57 +0200

    import: store / fix: component / add: template

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,16 +1,31 @@
 <script>
+import store from "../store";
 import Card from "./Card.vue";
+
 export default {
   data() {
     return {
-      Card,
+      store,
     };
+  },
+
+  components: {
+    Card,
   },
 };
 </script>
 
 <template>
-  <Card />
+  <ul>
+    <Card
+      v-for="card in store.movies"
+      :key="card.id"
+      :movieTitle="card.title"
+      :movieOriginalTitle="card.original_title"
+      :movieLanguage="card.original_language"
+      :movieVote="card.vote_average"
+    />
+  </ul>
 </template>
 
 <style lang="scss" scoped></style>
