1c1117cb1b39f8ec4e7589c8651c04a641cafe1a
GianluGiarde <gianlugiarde@gmail.com>
2023-05-25 15:43:20 +0200

    add: series list

--- src/components/CardsLists.vue
+++ src/components/CardsLists.vue
@@ -17,11 +17,11 @@ export default {
 
 <template>
   <ul>
-    <li><h2>Movies</h2></li>
+    <li v-if="store.movies.length != 0"><h2>Movies</h2></li>
     <Card v-for="card in store.movies" :key="card.id" :cardObj="card" />
   </ul>
   <ul>
-    <li><h2>Series</h2></li>
+    <li v-if="store.series.length != 0"><h2>Series</h2></li>
     <Card v-for="card in store.series" :key="card.id" :cardObj="card" />
   </ul>
 </template>
