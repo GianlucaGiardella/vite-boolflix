eb60ef42ec22b7a79e7988a9687b024b5a2ee232
GianluGiarde <gianlugiarde@gmail.com>
2023-05-25 17:16:20 +0200

    add: star rating

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,15 @@
 <script>
 export default {
+  data() {
+    return {
+      stars: 5,
+      starsNumber: Math.round(this.cardObj.vote_average / 2),
+    };
+  },
   props: {
     cardObj: Object,
   },
+  methods: {},
 };
 </script>
 
@@ -11,7 +18,11 @@ export default {
     {{ cardObj.title || cardObj.name }}
     {{ cardObj.original_title || cardObj.original_name }}
     <lang-flag :iso="cardObj.original_language" :squared="false" />
-    {{ cardObj.vote_average }}
+    <font-awesome-icon v-for="star in starsNumber" :icon="['fas', 'star']" />
+    <font-awesome-icon
+      v-for="star in stars - starsNumber"
+      :icon="['far', 'star']"
+    />
   </li>
 </template>
 
